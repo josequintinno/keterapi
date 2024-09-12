b7ab747c61b84150573e2168c5647bec54c5f486
Quintinno <josequintinno@icloud.com>
2024-09-12 17:33:57 -0300

    🔵 feature: Implementar informações sobre a API

--- src/main/java/br/com/quintinno/keterapi/Application.java
+++ src/main/java/br/com/quintinno/keterapi/Application.java
@@ -1,13 +1,36 @@
 package br.com.quintinno.keterapi;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+	private static final String SISTEMA = "Sistema Gerenciador de Associados";
+	private static final String VERSAO = "v1.0.0";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String api() {
+		return String.format("[KETERAPI%s] - %s | %s ", 
+        this.gerarNumeroChamado(), 
+        SISTEMA, 
+        VERSAO);
+	}
+
+	private String gerarNumeroChamado() {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+		return localDateTime.format(dateTimeFormatter);
 	}
 
 }
