0a01d6f1196ecec475c9c90119df7c03fd7c60c4
Quintinno <josequintinno@icloud.com>
2024-09-16 22:05:55 -0300

    🔵 feature: Implementar modelagem de dados dos Associados

--- src/main/resources/sql/modelagem.sql
+++ src/main/resources/sql/modelagem.sql
@@ -43,26 +43,34 @@ create table if not exists tb_documento (
 );
 
 create table if not exists tb_pessoa (
-	codigo
-	id_documento -- Todos os documentos da pessoa
-	id_contato -- Todos os contatos da pessoa
-	id_endereco -- Todos os endereços da pessoa
-	id_dependente -- Indica as pessoas que são dependentes (associados secundários) do associado principal
-	nome_completo -- Refere-se ao nome em caracteres ocidentais
-	nome_transcrito -- Refere-se ao nome transcrito de línguas que não possuem caracteres ocidentais
-	data_nascimento
-	data_associacao
-	e_ativo
+	codigo serial not null,
+	id_documento integer null,
+	id_contato integer not null,
+	id_endereco integer null,
+	id_dependente integer null,
+	nome_completo varchar(200) not null,
+	nome_transcrito varchar(200) null,
+	data_nascimento date null,
+	e_ativo boolean not null default true,
+	constraint pk_pessoa_codigo primary key (codigo),
+	constraint un_pessoa_nome unique (nome_completo, data_nascimento)
 );
 
 comment on table tb_pessoa is 'Armazena os dados de todos as pessoas associadas';
 comment on column tb_pessoa.codigo is 'Identificador Único da Tabela';
 comment on column tb_pessoa.nome_completo is 'Refere-se ao nome romanizado (caracteres latinos) da pessoa';
+comment on column tb_pessoa.id_documento is 'Refere-se aos documentos vinculados	 a uma determinada pessoa';
+comment on column tb_pessoa.id_contato is 'Refere-se aos contatos vinculados a uma determinada pessoa';
+comment on column tb_pessoa.id_endereco is 'Refere-se aos enderecos vinculados a uma determinada pessoa';
+comment on column tb_pessoa.id_dependente is 'Refere-se aos dependentes vinculados a uma determinada pessoa';
+comment on column tb_pessoa.nome_transcrito is 'Refere-se ao nome com caracteres originais da pessoa estrangeira';
+comment on column tb_pessoa.data_nascimento is 'Refere-se a data de nascimento da pessoa';
+comment on column tb_pessoa.e_ativo is 'Identifica se a pessoa esta ativa';
 
 create table if not exists tb_associado (
 	codigo
 	id_pessoa_associado_principal
-	id_pessoa_viculada
+	id_pessoa_dependente
 	id_relacionamento
 	data_inicio_vinculo
 	data_final_vinculo
@@ -71,7 +79,7 @@ create table if not exists tb_associado (
 comment on table tb_associado is 'Armazena os dados de todos as pessoas associadas';
 comment on column tb_associado.codigo is 'Identificador Único da Tabela';
 comment on column tb_associado.id_pessoa_associado_principal is 'Representa a pessoa responsável por uma determinada familia';
-comment on column tb_associado.id_pessoa_viculada is 'Representa a pessoa que está sendo vinculada com a pessoa principal';
+comment on column tb_associado.id_pessoa_dependente is 'Representa a pessoa que e dependente da pessoa principal';
 comment on column tb_associado.id_relacionamento is 'Representa o tipo de relacionamento entre as partes. Podem ser: RESPOSAVEL_FAMILIAR, ESPOSO, ESPOSA, FILHO, FILHA, MÃE, PAI, RESPONSÁVEL LEGAL, TIO, TIA, AVÔ, AVÓ, PADRASTRO, MADRASTRA, PROCURADOR';
 comment on column tb_associado.data_inicio_vinculo is 'Representa a data de inicio do vínculo como associado';
 comment on column tb_associado.data_final_vinculo is 'Representa a data de fim do vínculo como associado';
