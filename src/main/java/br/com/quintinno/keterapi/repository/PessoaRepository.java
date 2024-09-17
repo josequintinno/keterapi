b93ac3f0ea21e2786b79a86a0bf335873155583f
Quintinno <josequintinno@icloud.com>
2024-09-17 19:59:16 -0300

    🔵 feature: Implementar endpoint da funcionalidade de pessoas

--- src/main/java/br/com/quintinno/keterapi/repository/PessoaRepository.java
+++ src/main/java/br/com/quintinno/keterapi/repository/PessoaRepository.java
@@ -1,9 +1,13 @@
 package br.com.quintinno.keterapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.quintinno.keterapi.entity.PessoaEntity;
 
 @Repository
-public interface PessoaRepository extends JpaRepository<PessoaEntity, Long> { }
+public interface PessoaRepository extends JpaRepository<PessoaEntity, Long> {
+    List<PessoaEntity> findByAtivoTrue();
+}
