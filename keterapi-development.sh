d790924fc39f5b1083a3486fdb64cf295d071d37
Quintinno <josequintinno@icloud.com>
2024-09-18 10:23:36 -0300

    🟢 release: Configurar projeto para executar no ambiente de Desenvolvimento

--- keterapi-development.sh
+++ keterapi-development.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-mvn clean install -DskipTests
+echo "Construindo o projeto KETERAPI no ambiente de Desenvolvimento..."
+mvn clean install -DskipTests > keterapi-build.log 2>&1 &
 
 wait
 
-./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
+./mvnw spring-boot:run
+
+#./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
