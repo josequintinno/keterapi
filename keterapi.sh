6a7bf3dbb1ffc883c321718f052ee4abb82330eb
Quintinno <josequintinno@icloud.com>
2024-09-17 21:32:20 -0300

    🟢 release: Configurar projeto para executar em Produção

--- keterapi.sh
+++ keterapi.sh
@@ -4,8 +4,12 @@ set -e
 
 rm -rf target/
 
-echo "Instalando dependências e construindo o projeto..."
+echo "Instalando dependências e construindo o projeto KETERAPI"
 mvn clean install -DskipTests -X -U
 
-echo "Executando a aplicação..."
+echo "Executando a aplicação KETERAPI"
 java -jar target/keterapi-1.0.0-SNAPSHOT.jar --spring.profiles.active=production
+
+echo "APLICAÇÃO: Sistema Gerenciador de Associados"
+echo "VERSÃO: v1.0.0 | PORTA: 8000"
+echo "PORTA: 8000"
