d0b8c0e7d94a2d7256596737e6aba8a03f9e7c7c
Quintinno <josequintinno@icloud.com>
2024-09-17 22:05:05 -0300

    🟢 release: Configurar projeto para executar em Produção

--- keterapi.sh
+++ keterapi.sh
@@ -9,6 +9,9 @@ mvn clean install -DskipTests > build.log 2>&1 &
 
 wait
 
-echo "APLICAÇÃO: Sistema Gerenciador de Associados"
-echo "VERSÃO: v1.0.0 | PORTA: 8000"
+IP=$(hostname -I | awk '{print $1}')
+
+echo "APLICAÇÃO: KETER: Sistema Gerenciador de Associados"
+echo "VERSÃO: v1.0.0"
+echo "URL: http://$IP:8000/api/v1/"
 java -jar target/keterapi-1.0.0-SNAPSHOT.jar --spring.profiles.active=production
