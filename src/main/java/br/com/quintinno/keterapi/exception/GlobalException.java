4ea553bdeef778f30783418aeef93a1366bc8b76
Quintinno <josequintinno@icloud.com>
2024-09-18 12:14:34 -0300

    🔵 feature: Implementar tratamento de Erros ao realizar requisicoes aos endpoints da funcionalidade de cadastro de pessoas

--- src/main/java/br/com/quintinno/keterapi/exception/GlobalException.java
+++ src/main/java/br/com/quintinno/keterapi/exception/GlobalException.java
@@ -1,13 +1,16 @@
 package br.com.quintinno.keterapi.exception;
 
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import br.com.quintinno.keterapi.transfer.ExceptionTransfer;
 import br.com.quintinno.keterapi.utility.DateUtility;
@@ -22,6 +25,21 @@ public class GlobalException {
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<Object> httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException httpMediaTypeNotSupportedException) {
+        return new ResponseEntity<>(exceptionTransfer("Os dados passados na requisicao nao sao validos!", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException) {
+        return new ResponseEntity<>(exceptionTransfer("Os dados passados na requisicao nao sao validos!", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Object> noResourceFoundException(NoResourceFoundException noResourceFoundException) {
+        return new ResponseEntity<>(exceptionTransfer("Os dados passados na requisicao nao sao validos!", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> noSuchElementException(NoSuchElementException noSuchElementException) {
         return new ResponseEntity<>(exceptionTransfer("Os dados passados na requisicao nao sao validos!", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
