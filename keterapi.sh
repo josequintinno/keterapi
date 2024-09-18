d7bd6fbb66b9524ca66a9b67c9e346edff75e4e4
Quintinno <josequintinno@icloud.com>
2024-09-17 21:50:53 -0300

    🟢 release: Configurar projeto para executar em Produção

--- keterapi.sh
+++ keterapi.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf target/
 
-echo "Construindo o projeto KETERAPI"
+echo "Construindo o projeto KETERAPI..."
 mvn clean install -DskipTests > build.log 2>&1 &
 
 wait
@@ -12,12 +12,9 @@ wait
 if [ -f target/keterapi-1.0.0-SNAPSHOT.jar ]; then
     echo "Executando a aplicação KETERAPI..."
     java -jar target/keterapi-1.0.0-SNAPSHOT.jar --spring.profiles.active=production
+    echo "APLICAÇÃO: Sistema Gerenciador de Associados"
+    echo "VERSÃO: v1.0.0 | PORTA: 8000"
 else
     echo "Erro: O arquivo JAR não foi encontrado."
     exit 1
 fi
-
-wait
-
-echo "APLICAÇÃO: Sistema Gerenciador de Associados"
-echo "VERSÃO: v1.0.0 | PORTA: 8000"
