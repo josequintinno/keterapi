ddb511f81a7078ba04264cb9aea44d99b4a1a4d1
Quintinno <josequintinno@icloud.com>
2024-09-17 22:11:31 -0300

    🟢 release: Configurar projeto para executar em Produção

--- keterapi.sh
+++ keterapi.sh
@@ -10,10 +10,10 @@ mvn clean install -DskipTests > build.log 2>&1 &
 wait
 
 IP=$(hostname -I | awk '{print $1}')
-DATA=$(date +"%d/%m/%Y")
+DATA_HORA=$(date +"%d/%m/%Y %H:%M:%S")
 
 echo "APLICAÇÃO: KETER: Sistema Gerenciador de Associados"
 echo "VERSÃO: v1.0.0"
 echo "URL: http://$IP:8000/api/v1/"
-echo "DATA: $DATA"
+echo "DATA: $DATA_HORA"
 java -jar target/keterapi-1.0.0-SNAPSHOT.jar --spring.profiles.active=production
